e4af43d02a955ca359d28466a1f17ffd01f67205
T.Hrytsaienko <kortatiana89@gmail.com>
2018-03-04 16:09:47 +0200

    Changes in Header

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,47 +1,32 @@
+var tl = new TimelineMax({
+        paused: true,
+        onComplete: function () {
+            $('#burger').css("display", "block");
+            $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#cross">');
+            $('#menu-icon').removeClass('open');
+        },
+        onReverseComplete: function () {
+            $('#burger').css("display", "none");
+            $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#menu">');
+            $('#menu-icon').addClass('open');
+        }
+    });
+
+
+tl.fromTo('#burger', 1, {
+    autoAlpha: 0,
+    x: -1000
+}, {
+    autoAlpha: 1,
+    x: 0,
+    ease: Power2.easeOut
+});
+
+
 $('#menu-icon').click(function () {
     if ($('#menu-icon').hasClass('open')) {
-        TweenMax.to('#burger', 1, {
-            autoAlpha: 1,
-            x: 0,
-            ease: Power2.easeOut,
-            onComplete: function () {
-                $('#burger').css("display", "block");
-                $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#cross">');
-                $('#menu-icon').removeClass('open');
-            }
-        });
+        tl.play();
     } else {
-        TweenMax.from('#burger', 1, {
-            autoAlpha: 1,
-            x: 0,
-            ease: Power2.easeOut,
-            onComplete: function () {
-                $('#burger').css("display", "none");
-                $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#menu">');
-                $('#menu-icon').addClass('open');
-            }
-        });
+        tl.reverse();
     }
 })
-
-
-// $('#menu-icon').click(function () {
-//     $('#burger').css("display", "block");
-//     if ($('#menu-icon').hasClass('open')) {
-//         $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#cross">');
-//         $('#menu-icon').removeClass('open');
-//         TweenMax.to('#burger', 1, {
-//             autoAlpha: 1,
-//             x: 0,
-//             ease: Power2.easeOut
-//         });
-//     } else {
-//         TweenMax.from('#burger', 5, {
-//             autoAlpha: 1,
-//             x: 0
-//         });
-//         $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#menu">');
-//         $('#menu-icon').addClass('open');
-//         $('#burger').css("display", "none");
-//     }
-// })
