c2f7645d56d70fd732de333cbc338c517adcec31
T.Hrytsaienko <kortatiana89@gmail.com>
2018-03-09 23:04:01 +0200

    Images added

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,4 @@ import '../blocks/footer/footer';
 import '../blocks/header/header';
 import '../blocks/menu/menu';
 import '../blocks/social-icons/social-icons';
+import '../blocks/gallery-content/gallery-content';
