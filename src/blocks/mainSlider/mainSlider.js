bc763075e20f61abcda6f85d16f4254c6d86189a
roma-kohanets <r.kohanets@gmail.com>
2018-03-14 19:16:00 +0200

    main slider done

---
+++ src/blocks/mainSlider/mainSlider.js
@@ -0,0 +1,12 @@
+$('.carousel').slick({
+  arrows: false
+})
+
+
+$('.carousel-container__button-prev').click(function(){
+  $('.carousel').slick('slickPrev');
+})
+
+ $('.carousel-container__button-next').click(function(){
+   $('.carousel').slick('slickNext');
+ })
