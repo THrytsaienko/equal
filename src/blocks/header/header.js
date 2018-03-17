a7a354a3aa6afd75246c0397f5aa9cbb2ee2762a
T.Hrytsaienko <kortatiana89@gmail.com>
2018-03-17 02:00:10 +0200

    Changed menu-icon and animation

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,79 +1,80 @@
-var tl = new TimelineMax({
-	paused: true,
-	onComplete: function () {
-		// $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#cross">');
-		// $('#menu-icon').removeClass('open').addClass('header__menu-icon-close');
-	},
-	onReverseComplete: function () {
-		$('#burger').css("display", "none");
-		// $('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#menu">');
-		// $('#menu-icon').addClass('open').removeClass('header__menu-icon-close');
-	}
-});
+$(function () {    
+	var headerInit = function (event, page) {        
+		if (page == undefined) page = document;        
+		$page = $(page);        
+		$block = $page.find('.header');        
+		if (!$block.length) return;        
+		$menuIcon = $block.find('.header__menu-burger');        
+		$burger = $block.find('.header__burger');        
+		var tl = new TimelineMax({            
+			paused: true,
+			onReverseComplete: function () {                
+				$burger.css("display", "none");         
+			}        
+		});        
+		tl.set($burger, {            
+			'display': 'block'        
+		});        
+		tl.fromTo($burger, 1, {            
+			autoAlpha: 0,
+			x: -1000        
+		}, {            
+			autoAlpha: 1,
+			x: 0,
+			ease: Power2.easeOut        
+		});
 
-tl.set('#burger', {
-	'display': 'block'
-});
-tl.fromTo('#burger', 1, {
-	autoAlpha: 0,
-	x: -1000
-}, {
-	autoAlpha: 1,
-	x: 0,
-	ease: Power2.easeOut
-});
+		$iconTop = $('.header__menu-icon-top');
+		$iconMiddle = $('.header__menu-icon-middle');
+		$iconBottom = $('.header__menu-icon-bottom');
 
+		var tlIconMenuTop = new TimelineMax({
+			paused: true
+		});
 
-$('#menu-icon').click(function () {
-	if ($('#menu-icon').hasClass('open')) {
-		$('#menu-icon').removeClass('open').addClass('header__menu-icon-close');
-		$('.header__menu-icon__middle').removeClass('open').addClass('header__menu-icon__middle-close');
-		tl.play();
-	} else {
-		$('#menu-icon').addClass('open').removeClass('header__menu-icon-close');
-		$('.header__menu-icon__middle-close').addClass('open').removeClass('header__menu-icon__middle-close');
-		tl.reverse();
-	}
-});
+		var tlIconMenuMiddle = new TimelineMax({
+			paused: true
+		});
 
-// var tlIcon = new TimelineMax({});
+		var tlIconMenuBottom = new TimelineMax({
+			paused: true
+		});
 
-// tlIcon.fromTo('.header__menu-icon__middle', 1, {
-// 	x: 0
-// }, {
-// 	x: -1000,
-// 	ease: Power2.easeOut
-// });
+		tlIconMenuTop.to($iconTop, .5, {
+			rotation: '45deg',
+			ease: Power2.easeOut
+		});   
 
-
-// var tl = new TimelineMax({
-// 		paused: true,
-// 		onComplete: function () {
-// 			$('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#cross">');
-// 			$('#menu-icon').removeClass('open');
-// 		},
-// 		onReverseComplete: function () {
-// 			$('#burger').css("display", "none");
-// 			$('#menu-icon').html('<use xlink:href="/assets/svg/symbol/svg/sprite.symbol.svg#menu">');
-// 			$('#menu-icon').addClass('open');
-// 		}
-// 	});
-
-// tl.set('#burger', { 'display': 'block' });
-// tl.fromTo('#burger', 1, {
-// 	autoAlpha: 0,
-// 	x: -1000
-// }, {
-// 	autoAlpha: 1,
-// 	x: 0,
-// 	ease: Power2.easeOut
-// });
-
-
-// $('#menu-icon').click(function () {
-// 	if ($('#menu-icon').hasClass('open')) {
-// 		tl.play();
-// 	} else {
-// 		tl.reverse();
-// 	}
-// });
+		tlIconMenuMiddle
+			.to($iconMiddle, .2, {
+				autoAlpha: 0
+			})
+			.to($iconMiddle, .5, {
+				x: -500
+			}, .2); 
+			  
+		tlIconMenuBottom.to($iconBottom, .5, {
+			rotation: '-45deg',
+			y: 1,
+			ease: Power2.easeOut
+		});     
+		   
+		$menuIcon.click(function () {            
+			if ($(this).hasClass('open')) {  
+				$menuIcon.removeClass('open');              
+				tl.play();            
+				tlIconMenuTop.play();            
+				tlIconMenuMiddle.play();            
+				tlIconMenuBottom.play();            
+			} else {
+				$menuIcon.addClass('open');           
+				tl.reverse();            
+				tlIconMenuTop.reverse();            
+				tlIconMenuMiddle.reverse();            
+				tlIconMenuBottom.reverse();            
+			}        
+		});    
+	};    
+	headerInit();    
+	$(document).on("pageInit", headerInit);
+});   
