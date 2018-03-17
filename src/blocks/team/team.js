8e7fd3e58a553eb4a0d580eb3317061ce675c113
Seitibraimova Lemara <lemarasd@gmail.com>
2018-03-17 22:08:19 +0200

    carousel

--- src/blocks/team/team.js
+++ src/blocks/team/team.js
@@ -1,5 +1,22 @@
-// =============== b-clients script ===============
-$(document).ready(function(){
-	// console.log("clients msg")
+$(function() {
+  var $block;
+  $block = $('.team-slider');
+  if (!$block.length) return;
+  $container = $block.find('.team-slider__slides');
+
+  $slides.slick({
+    arrows: false,
+  });
+
+  $prevButton = $('.team-slider__button-prev');
+  $nextButton = $('.team-slider__button-next');
+
+  $prevButton.click(function(){
+    $(this).closest($block).find($slides).slick('slickPrev');
+  })
+
+   $nextButton.click(function(){
+     $(this).closest($block).find($slides).slick('slickNext');
+   })  
 })
-// =============== b-clients script ===============
+
